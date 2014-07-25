1910e834fb5f2a89a85c28325cc88526239f3427
Cliff <cliff@hitomedia.jp>
2014-07-25 19:18:27 +0900

    changes

--- index.php
+++ index.php
@@ -3,8 +3,22 @@
 <!doctype html>
 <html>
 	<head>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/index.css">
+		<link href="http://fonts.googleapis.com/css?family=Poiret+One:400" rel="stylesheet" type="text/css">
+		<script src="js/jquery-2.1.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
 	</head>
 	<body>
+		<ul>
+			<a href="https://www.facebook.com/cliff.tanaka" target="_blank"><li class="facebook"></li></a>
+			<a href="https://twitter.com/tanakuri" target="_blank"><li class="twitter"></li></a>
+			<a href="http://www.linkedin.com/profile/view?id=47786198" target="_blank"><li class="linkedin"></li></a>
+			<a href="https://plus.google.com/u/1/117253355975298139842/posts" target="_blank"><li class="google"></li></a>
+			<a href="http://www.pinterest.com/kurifu/" target="_blank"><li class="pinterest"></li></a>
+			<a href="http://instagram.com/kurifu" target="_blank"><li class="instagram"></li></a>
+		</ul>
+		<div>cliff<span>.tokyo</span></div>
 	</body>
 </html>
 	
