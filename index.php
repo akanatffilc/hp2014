9abc67e1d16f0dae3390736ed004dc068c63f17b
Cliff <cliff@hitomedia.jp>
2014-07-25 19:27:58 +0900

    changes

--- index.php
+++ index.php
@@ -3,11 +3,25 @@
 <!doctype html>
 <html>
 	<head>
+		<meta charset="utf-8">
+	  	<title>cliff.tokyo</title>
+	  	<meta name="keywords" content="">
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no,maximum-scale=1">
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/index.css">
 		<link href="http://fonts.googleapis.com/css?family=Poiret+One:400" rel="stylesheet" type="text/css">
 		<script src="js/jquery-2.1.1.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-53199073-1', 'auto');
+		  ga('send', 'pageview');
+		</script>
 	</head>
 	<body>
 		<ul>
